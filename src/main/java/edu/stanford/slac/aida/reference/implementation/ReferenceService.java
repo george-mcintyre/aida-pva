a7d60d6204ad51c27b2dae02f396e5bc8bdf3842
george-mcintyre <george@level-n.com>
2021-07-16 12:28:35 +0200

    Load library name from environment or property by default

--- src/main/java/edu/stanford/slac/aida/reference/implementation/ReferenceService.java
+++ src/main/java/edu/stanford/slac/aida/reference/implementation/ReferenceService.java
@@ -3,8 +3,17 @@ package edu.stanford.slac.aida.reference.implementation;
 import edu.stanford.slac.aida.lib.AidaProviderRunner;
 
 public class ReferenceService {
+    private final static String AIDA_PVA_LIB_NAME = "aida-pva";
+
     static {
-        System.loadLibrary("aida-pva-reference");
+        // Get library name from property or environment.
+        String aidaPvaLibName = System.getProperty("aida.pva.lib.name", AIDA_PVA_LIB_NAME);
+        String aidaPvaLibEnv = System.getenv("AIDA_PVA_LIB_NAME");
+        if (aidaPvaLibEnv != null) {
+            aidaPvaLibName = aidaPvaLibEnv;
+        }
+
+        System.loadLibrary(aidaPvaLibName);
     }
 
     public static void main(String[] args) {
