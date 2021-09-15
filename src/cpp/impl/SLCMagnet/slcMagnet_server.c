68cadaa4d34371bbdd0c471c9a1e17c11ec52e3c
george-mcintyre <george@level-n.com>
2021-09-15 23:34:16 +0200

    Add magnet service

---
+++ src/cpp/impl/SLCMagnet/slcMagnet_server.c
@@ -0,0 +1,332 @@
+/**
+ * Klystron Server implementation
+ */
+#include <string.h>
+#include <stdlib.h>
+
+#include "aida_server_helper.h"
+#include "json.h"
+
+#include "slcMagnet_server.h"
+
+#include "slc_macros.h"           /* vmsstat_t, int2u, int4u, etc. */
+
+/**
+ * Initialise the service
+ * @param env to be used to throw exceptions using aidaThrow() and aidaNonOsExceptionThrow()
+ * @throws ServerInitialisationException if the service fails to initialise
+ */
+void aidaServiceInit(JNIEnv* env)
+{
+	printf("Aida Klystron Service Initialised\n");
+}
+
+/**
+ * Get channel configuration
+ * @param channelName
+ * @param env to be used to throw exceptions using aidaThrow() and aidaNonOsExceptionThrow()
+ * @return the channel config.  Leave empty if you don't have any specific configuration overrides
+ */
+Config aidaChannelConfig(JNIEnv* env, const char* channelName)
+{
+	Config config;
+	memset(&config, 0, sizeof(config));
+	return config;
+}
+
+/**
+ * Get a boolean
+ *
+ * @param env to be used to throw exceptions using aidaThrow() and aidaNonOsExceptionThrow()
+ * @param uri the uri
+ * @param arguments the arguments
+ * @return the boolean
+ */
+int aidaRequestBoolean(JNIEnv* env, const char* uri, Arguments arguments)
+{
+	aidaThrowNonOsException(env, UNSUPPORTED_CHANNEL_EXCEPTION, uri);
+	return 0;
+}
+
+/**
+ * Get a byte
+ *
+ * @param env to be used to throw exceptions using aidaThrow() and aidaNonOsExceptionThrow()
+ * @param uri the uri
+ * @param arguments the arguments
+ * @return the byte
+ */
+char aidaRequestByte(JNIEnv* env, const char* uri, Arguments arguments)
+{
+	aidaThrowNonOsException(env, UNSUPPORTED_CHANNEL_EXCEPTION, uri);
+	return 0x0;
+}
+
+/**
+ * Get a short
+ *
+ * @param uri the uri
+ * @param env to be used to throw exceptions using aidaThrow() and aidaNonOsExceptionThrow()
+ * @param arguments the arguments
+ * @return the short
+ */
+short aidaRequestShort(JNIEnv* env, const char* uri, Arguments arguments)
+{
+	aidaThrowNonOsException(env, UNSUPPORTED_CHANNEL_EXCEPTION, uri);
+	return 0;
+}
+
+/**
+ * Get a integer
+ *
+ * @param env to be used to throw exceptions using aidaThrow() and aidaNonOsExceptionThrow()
+ * @param uri the uri
+ * @param arguments the arguments
+ * @return the integer
+ */
+int aidaRequestInteger(JNIEnv* env, const char* uri, Arguments arguments)
+{
+	aidaThrowNonOsException(env, UNSUPPORTED_CHANNEL_EXCEPTION, uri);
+	return 0;
+}
+
+/**
+ * Get a long
+ *
+ * @param env to be used to throw exceptions using aidaThrow() and aidaNonOsExceptionThrow()
+ * @param uri the uri
+ * @param arguments the arguments
+ * @return the long
+ */
+long aidaRequestLong(JNIEnv* env, const char* uri, Arguments arguments)
+{
+	aidaThrowNonOsException(env, UNSUPPORTED_CHANNEL_EXCEPTION, uri);
+	return 0;
+}
+
+/**
+ * Get a float
+ *
+ * @param env to be used to throw exceptions using aidaThrow() and aidaNonOsExceptionThrow()
+ * @param uri the uri
+ * @param arguments the arguments
+ * @return the float
+ */
+float aidaRequestFloat(JNIEnv* env, const char* uri, Arguments arguments)
+{
+	aidaThrowNonOsException(env, UNSUPPORTED_CHANNEL_EXCEPTION, uri);
+	return 0.0f;
+}
+
+/**
+ * Get a double
+ *
+ * @param env to be used to throw exceptions using aidaThrow() and aidaNonOsExceptionThrow()
+ * @param uri the uri
+ * @param arguments the arguments
+ * @return the double
+ */
+double aidaRequestDouble(JNIEnv* env, const char* uri, Arguments arguments)
+{
+	aidaThrowNonOsException(env, UNSUPPORTED_CHANNEL_EXCEPTION, uri);
+	return 0.0;
+}
+
+/**
+ * Get a string.  Allocate memory for string and it will be freed for you by framework
+ *
+ * @param env to be used to throw exceptions using aidaThrow() and aidaNonOsExceptionThrow()
+ * @param uri the uri
+ * @param arguments the arguments
+ * @return the string
+ */
+char* aidaRequestString(JNIEnv* env, const char* uri, Arguments arguments)
+{
+	aidaThrowNonOsException(env, UNSUPPORTED_CHANNEL_EXCEPTION, uri);
+	return NULL;
+}
+
+/**
+ * Get a boolean array
+ *
+ * @param env to be used to throw exceptions using aidaThrow() and aidaNonOsExceptionThrow()
+ * @param uri the uri
+ * @param arguments the arguments
+ * @return the boolean array
+ */
+Array aidaRequestBooleanArray(JNIEnv* env, const char* uri, Arguments arguments)
+{
+	Array array;
+	array.count = 0;
+	array.items = NULL;
+	aidaThrowNonOsException(env, UNSUPPORTED_CHANNEL_EXCEPTION, uri);
+	return array;
+}
+
+/**
+ * Get a byte array
+ *
+ * @param env to be used to throw exceptions using aidaThrow() and aidaNonOsExceptionThrow()
+ * @param uri the uri
+ * @param arguments the arguments
+ * @return the byte array
+ */
+Array aidaRequestByteArray(JNIEnv* env, const char* uri, Arguments arguments)
+{
+	Array array;
+	array.count = 0;
+	array.items = NULL;
+	aidaThrowNonOsException(env, UNSUPPORTED_CHANNEL_EXCEPTION, uri);
+	return array;
+}
+
+/**
+ * Get a short array
+ *
+ * @param env to be used to throw exceptions using aidaThrow() and aidaNonOsExceptionThrow()
+ * @param uri the uri
+ * @param arguments the arguments
+ * @return the short array
+ */
+Array aidaRequestShortArray(JNIEnv* env, const char* uri, Arguments arguments)
+{
+	Array array;
+	array.count = 0;
+	array.items = NULL;
+	aidaThrowNonOsException(env, UNSUPPORTED_CHANNEL_EXCEPTION, uri);
+	return array;
+}
+
+/**
+ * Get a integer array
+ *
+ * @param env to be used to throw exceptions using aidaThrow() and aidaNonOsExceptionThrow()
+ * @param uri the uri
+ * @param arguments the arguments
+ * @return the integer array
+ */
+Array aidaRequestIntegerArray(JNIEnv* env, const char* uri, Arguments arguments)
+{
+	Array array;
+	array.count = 0;
+	array.items = NULL;
+	aidaThrowNonOsException(env, UNSUPPORTED_CHANNEL_EXCEPTION, uri);
+	return array;
+}
+
+/**
+ * Get a long array
+ *
+ * @param env to be used to throw exceptions using aidaThrow() and aidaNonOsExceptionThrow()
+ * @param uri the uri
+ * @param arguments the arguments
+ * @return the long array
+ */
+Array aidaRequestLongArray(JNIEnv* env, const char* uri, Arguments arguments)
+{
+	Array array;
+	array.count = 0;
+	array.items = NULL;
+	aidaThrowNonOsException(env, UNSUPPORTED_CHANNEL_EXCEPTION, uri);
+	return array;
+}
+
+/**
+ * Get a float array
+ *
+ * @param env to be used to throw exceptions using aidaThrow() and aidaNonOsExceptionThrow()
+ * @param uri the uri
+ * @param arguments the arguments
+ * @return the float array
+ */
+Array aidaRequestFloatArray(JNIEnv* env, const char* uri, Arguments arguments)
+{
+	Array array;
+	array.count = 0;
+	array.items = NULL;
+	aidaThrowNonOsException(env, UNSUPPORTED_CHANNEL_EXCEPTION, uri);
+	return array;
+}
+
+/**
+ * Get a double array
+ *
+ * @param env to be used to throw exceptions using aidaThrow() and aidaNonOsExceptionThrow()
+ * @param uri the uri
+ * @param arguments the arguments
+ * @return the double array
+ */
+Array aidaRequestDoubleArray(JNIEnv* env, const char* uri, Arguments arguments)
+{
+	Array array;
+	array.count = 0;
+	array.items = NULL;
+	aidaThrowNonOsException(env, UNSUPPORTED_CHANNEL_EXCEPTION, uri);
+	return array;
+}
+
+/**
+ * Get a string array
+ *
+ * @param env to be used to throw exceptions using aidaThrow() and aidaNonOsExceptionThrow()
+ * @param uri the uri
+ * @param arguments the arguments
+ * @return the string array
+ */
+StringArray aidaRequestStringArray(JNIEnv* env, const char* uri, Arguments arguments)
+{
+	StringArray stringArray;
+	stringArray.count = 0;
+	aidaThrowNonOsException(env, UNSUPPORTED_CHANNEL_EXCEPTION, uri);
+	return stringArray;
+}
+
+/**
+ * Get a table of data
+ *
+ * @param env to be used to throw exceptions using aidaThrow() and aidaNonOsExceptionThrow()
+ * @param uri the uri
+ * @param arguments the arguments
+ * @return the table
+ */
+Table aidaRequestTable(JNIEnv* env, const char* uri, Arguments arguments)
+{
+	Table table;
+	memset(&table, 0, sizeof(table));
+	table.columnCount = 0;
+	aidaThrowNonOsException(env, UNSUPPORTED_CHANNEL_EXCEPTION, uri);
+	return table;
+}
+
+/**
+ * Set a value
+ *
+ * @param env to be used to throw exceptions using aidaThrow() and aidaNonOsExceptionThrow()
+ * @param uri the uri
+ * @param arguments the arguments
+ * @param value to set
+ * @return the table
+ */
+void aidaSetValue(JNIEnv* env, const char* uri, Arguments arguments, Value value)
+{
+	aidaThrowNonOsException(env, UNSUPPORTED_CHANNEL_EXCEPTION, uri);
+}
+
+/**
+ * Set a value and return a table as a response
+ *
+ * @param env to be used to throw exceptions using aidaThrow() and aidaNonOsExceptionThrow()
+ * @param uri the uri
+ * @param arguments the arguments
+ * @param value to set
+ * @return a table
+ */
+Table aidaSetValueWithResponse(JNIEnv* env, const char* uri, Arguments arguments, Value value)
+{
+	Table table;
+	memset(&table, 0, sizeof(table));
+	table.columnCount = 0;
+	aidaThrowNonOsException(env, UNSUPPORTED_CHANNEL_EXCEPTION, uri);
+	return table;
+}
+
