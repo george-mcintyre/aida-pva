762e8c7b537634fbae8d857d2f3a23d37079838c
george-mcintyre <george@level-n.com>
2022-01-31 14:50:19 +0100

    Add support for ALIAS channels

--- src/main/java/edu/stanford/slac/aida/lib/RequestExecutor.java
+++ src/main/java/edu/stanford/slac/aida/lib/RequestExecutor.java
@@ -2,22 +2,9 @@ package edu.stanford.slac.aida.lib;
 
 import org.epics.pvaccess.client.rpc.RPCClientImpl;
 import org.epics.pvaccess.server.rpc.RPCRequestException;
-import org.epics.pvdata.factory.FieldFactory;
-import org.epics.pvdata.pv.FieldCreate;
 import org.epics.pvdata.pv.PVStructure;
 
 public class RequestExecutor {
-    /**
-     * To be able to create fields in PV Access you need a field creator factory
-     */
-    private final static FieldCreate fieldCreate = FieldFactory.getFieldCreate();
-
-    /**
-     * Normative Type ID string for URIs
-     */
-    public static final String NTURI_ID = "epics:nt/NTURI:1.0";
-
-
     static PVStructure executeRequest(PVStructure pvUri, String channelName) throws RPCRequestException {
         RPCClientImpl client = null;
         try {
